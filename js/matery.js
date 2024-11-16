d13044c18dbc7d69e58e2526f6e7985d120d0881
wylblog <wylblog2794998160@qq.com>
2024-11-16 17:19:04 +0800

    Site updated: 2024-11-16 17:19:03

--- js/matery.js
+++ js/matery.js
@@ -106,6 +106,11 @@ $(function () {
             subHtmlSelectorRelative: true
         });
 
+        // 优化懒加载与其他插件冲突
+        $(document).find('img[data-original]').each(function(){
+            $(this).parent().attr("href", $(this).attr("data-original"));
+        });
+
         // progress bar init
         const progressElement = window.document.querySelector('.progress-bar');
         if (progressElement) {
